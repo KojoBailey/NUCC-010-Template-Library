bab7562937d45f58e6df610826893975acdd072d
Kojo Bailey <kojomolojo@gmail.com>
2024-04-22 17:29:16 +0300

    Initial commit

---
+++ All-Star Battle R/Character_IDs.h
@@ -0,0 +1,593 @@
+string CharIdToShortName(string CharID) {
+    switch (CharID) {
+        case "0bao01":
+            return "Baoh";
+        case "0hzi01":
+            return "Ikuro";
+    // Part 1: Phantom Blood
+        case "1dio01":
+            return "Dio";
+        case "1jnt01":
+            return "Jonathan";
+        case "1sdw01":
+            return "Speedwagon";
+        case "1zpl01":
+            return "Will";
+        case "1nkm01":
+            return "Kempo";
+        case "1nkm02":
+            return "Tattoo";
+    // Part 2: Battle Tendency
+        case "2csr01":
+            return "Caesar";
+        case "2esd01":
+            return "Esidisi";
+        case "2jsp01":
+            return "Joseph";
+        case "2lsa01":
+            return "Lisa Lisa";
+        case "2krs01":
+            return "Kars";
+        case "2krs02":
+            return "Ulti Kars";
+        case "2shm01":
+            return "Stroheim";
+        case "2wmu01":
+            return "Wamuu";
+    // Part 3: Stardust Crusaders
+        case "3abd01":
+            return "Avdol";
+        case "3dio01":
+            return "DIO";
+        case "3hhs01":
+            return "Hol Horse";
+        case "3igy01":
+            return "Iggy";
+        case "3jsp01":
+            return "Old Joseph";
+        case "3jtr01":
+            return "Jotaro";
+        case "3kki01":
+            return "Kakyoin";
+        case "3mra01":
+            return "Mariah";
+        case "3pln01":
+            return "Polnareff";
+        case "3psp01":
+            return "Pet Shop";
+        case "3vni01":
+            return "Vanilla Ice";
+        case "3bst01":
+            return "Bast";
+        case "3crm01":
+            return "Cream";
+        case "3fll01":
+            return "Fool";
+        case "3hdm01":
+            return "Hanged Man";
+        case "3heg01":
+            return "Hierophant";
+        case "3hru01":
+            return "Hermit";
+        case "3mgr01":
+            return "Magician";
+        case "3slc01":
+            return "Chariot";
+        case "3stp01":
+            return "Platinum";
+        case "3wld01":
+            return "World";
+    // Part 4: Diamond is Unbreakable
+        case "4fgm01":
+            return "Yuya";
+        case "4jsk01":
+            return "Josuke 4";
+        case "4jtr01":
+            return "Jotaro 4";
+        case "4kch01":
+            return "Keicho";
+        case "4kir01":
+            return "Kira";
+        case "4koi01":
+            return "Koichi";
+        case "4kwk01":
+            return "Kosaku";
+        case "4oky01":
+            return "Okuyasu";
+        case "4oti01":
+            return "Akira";
+        case "4rhn01":
+            return "Rohan";
+        case "4sgc01":
+            return "Shigechi";
+        case "4ykk01":
+            return "Yukako";
+        case "4bco01":
+            return "Company";
+        case "4crd01":
+            return "Diamond";
+        case "4ech01":
+            return "Echoes 1";
+        case "4ech02":
+            return "Echoes 2";
+        case "4ech03":
+            return "Echoes 3";
+        case "4hdr01":
+            return "Heaven's Door";
+        case "4hnd01":
+            return "Hand";
+        case "4hvt01":
+            return "Harvest";
+        case "4hws01":
+            return "Highway Star";
+        case "4klq01":
+            return "Killer Queen";
+        case "4kwh01":
+            return "Hayato";
+        case "4rhc01":
+            return "Pepper";
+        case "4stp01":
+            return "Platinum 4";
+    // Part 5: Vento Aureo / Golden Wind
+        case "5abc01":
+            return "Abbacchio";
+        case "5bct01":
+            return "Bucciarati";
+        case "5dvl01":
+            return "Diavolo";
+        case "5fgo01":
+            return "Fugo";
+        case "5gac01":
+            return "Ghiaccio";
+        case "5grn01":
+            return "Giorno";
+        case "5mst01":
+            return "Mista";
+        case "5nrc01":
+            return "Narancia";
+        case "5prs01":
+            return "Prosciutto";
+        case "5psc01":
+            return "Pesci";
+        case "5ris01":
+            return "Risotto";
+        case "5trs01":
+            return "Trish";
+
+        case "5ars01":
+            return "Aerosmith";
+        case "5dpo01":
+            return "Doppio";
+        case "5gex01":
+            return "Gold Experience";
+        case "5gfd01":
+            return "Grateful Dead";
+        case "5gxr01":
+            return "GER";
+        case "5kcr01":
+            return "Crimson";
+        case "5mdb01":
+            return "Moody Blues";
+        case "5mtl01":
+            return "Metallica";
+        case "5pph01":
+            return "Purple Haze";
+        case "5pph02":
+            return "Purple Haze D";
+        case "5sfg01":
+            return "Sticky";
+        case "5spg01":
+            return "Spice";
+        case "5sxp01":
+            return "Pistols #1";
+        case "5sxp02":
+            return "Pistols #2";
+        case "5sxp03":
+            return "Pistols #3";
+        case "5sxp05":
+            return "Pistols #5";
+        case "5sxp06":
+            return "Pistols #6";
+        case "5sxp07":
+            return "Pistols #7";
+        case "5wha01":
+            return "Album";
+    // Part 6: Stone Ocean
+        case "6ans01":
+            return "Anasui";
+        case "6elm01":
+            return "Ermes";
+        case "6fit01":
+            return "F.F.";
+        case "6jln01":
+            return "Jolyne";
+        case "6pci01":
+            return "Pucci Final";
+        case "6pci02":
+            return "Pucci WS";
+        case "6wet01":
+            return "Weather";
+        case "6cmo01":
+            return "C-MOON";
+        case "6ddw01":
+            return "Diver Down";
+        case "6kis01":
+            return "Kiss";
+        case "6mih01":
+            return "MiH";
+        case "6ptn01":
+            return "Foo Fighters";
+        case "6stf01":
+            return "Stone";
+        case "6whs01":
+            return "Whitesnake";
+        case "6wrp01":
+            return "Weather Report";
+    // Part 7: Steel Ball Run
+        case "7dio01":
+            return "Diego";
+        case "7dio02":
+            return "PL Diego";
+        case "7jir01":
+            return "Gyro";
+        case "7jny01":
+            return "Johnny";
+        case "7vtn01":
+            return "Valentine";
+        case "7bbr01":
+            return "Breaker";
+        case "7blt01":
+            return "Silver Bullet";
+        case "7cps01":
+            return "Corpse";
+        case "7dfc01":
+            return "D4C";
+        case "7scm01":
+            return "Scary Monsters";
+        case "7sld01":
+            return "Slow Dancer";
+        case "7tsk01":
+            return "Tusk 1";
+        case "7tsk02":
+            return "Tusk 2";
+        case "7tsk03":
+            return "Tusk 3";
+        case "7tsk04":
+            return "Tusk 4";
+        case "7val01":
+            return "Valkyrie";
+        case "7wld01":
+            return "WORLD";
+    // Part 8: JoJolion
+        case "8jsk01":
+            return "Josuke 8";
+        case "8tor01":
+            return "Toru";
+        case "8saw01":
+            return "Soft & Wet";
+        case "8wou01":
+            return "Wonder of U";
+    // If none matched
+        default:
+            return CharID;
+    }
+}
+
+string CharIdToFullName(string CharID) {
+    switch (CharID) {
+        case "0bao01":
+            return "Baoh";
+        case "0hzi01":
+            return "Ikuro Hashizawa";
+    // Part 1: Phantom Blood
+        case "1dio01":
+            return "Dio Brando";
+        case "1jnt01":
+            return "Jonathan Joestar";
+        case "1sdw01":
+            return "Robert E. O. Speedwagon";
+        case "1zpl01":
+            return "Will A. Zeppeli";
+        case "1nkm01":
+            return "Kempo Figher";
+        case "1nkm02":
+            return "Tattoo";
+    // Part 2: Battle Tendency
+        case "2csr01":
+            return "Caesar Anthonio Zeppeli";
+        case "2esd01":
+            return "Esidisi";
+        case "2jsp01":
+            return "Joseph Joestar";
+        case "2lsa01":
+            return "Lisa Lisa";
+        case "2krs01":
+            return "Kars";
+        case "2krs02":
+            return "Ultimate Kars";
+        case "2shm01":
+            return "Rudol von Stroheim";
+        case "2wmu01":
+            return "Wamuu";
+    // Part 3: Stardust Crusaders
+        case "3abd01":
+            return "Muhammad Avdol";
+        case "3dio01":
+            return "DIO";
+        case "3hhs01":
+            return "Hol Horse";
+        case "3igy01":
+            return "Iggy";
+        case "3jsp01":
+            return "Old Joseph Joestar";
+        case "3jtr01":
+            return "Jotaro Kujo";
+        case "3kki01":
+            return "Noriaki Kakyoin";
+        case "3mra01":
+            return "Mariah";
+        case "3pln01":
+            return "Jean Pierre Polnareff";
+        case "3psp01":
+            return "Pet Shop";
+        case "3vni01":
+            return "Vanilla Ice";
+        case "3bst01":
+            return "Bast";
+        case "3crm01":
+            return "Cream";
+        case "3fll01":
+            return "The Fool";
+        case "3hdm01":
+            return "Hanged Man";
+        case "3heg01":
+            return "Hierophant Green";
+        case "3hru01":
+            return "Hermit Purple";
+        case "3mgr01":
+            return "Magician's Red";
+        case "3slc01":
+            return "Silver Chariot";
+        case "3slc03":
+            return "Silver Chariot (Armourless)";
+        case "3stp01":
+            return "Star Platinum";
+        case "3wld01":
+            return "The World";
+    // Part 4: Diamond is Unbreakable
+        case "4fgm01":
+            return "Yuya Fungami";
+        case "4jsk01":
+            return "Josuke Higashikata 4";
+        case "4jtr01":
+            return "Jotaro Kujo (Part 4)";
+        case "4kch01":
+            return "Keicho Nijimura";
+        case "4kir01":
+            return "Yoshikage Kira";
+        case "4koi01":
+            return "Koichi Hirose";
+        case "4kwk01":
+            return "Kosaku Kawajiri";
+        case "4oky01":
+            return "Okuyasu Nijimura";
+        case "4oti01":
+            return "Akira Otoishi";
+        case "4rhn01":
+            return "Rohan Kishibe";
+        case "4sgc01":
+            return "Shigekiyo Yangu";
+        case "4ykk01":
+            return "Yukako Yamagishi";
+        case "4bco01":
+            return "Bad Company";
+        case "4crd01":
+            return "Crazy Diamond";
+        case "4ech01":
+            return "Echoes ACT1";
+        case "4ech02":
+            return "Echoes ACT2";
+        case "4ech03":
+            return "Echoes ACT3";
+        case "4hdr01":
+            return "Heaven's Door";
+        case "4hnd01":
+            return "The Hand";
+        case "4hvt01":
+            return "Harvest";
+        case "4hws01":
+            return "Highway Star";
+        case "4klq01":
+            return "Killer Queen";
+        case "4klq02":
+            return "Killer Queen (Kosaku)";
+        case "4kwh01":
+            return "Hayato Kawajiri";
+        case "4rhc01":
+            return "Red Hot Chili Pepper";
+        case "4stp01":
+            return "Star Platinum (Part 4)";
+    // Part 5: Vento Aureo / Golden Wind
+        case "5abc01":
+            return "Leone Abbacchio";
+        case "5bct01":
+            return "Bruno Bucciarati";
+        case "5dvl01":
+            return "Diavolo";
+        case "5fgo01":
+            return "Pannacotta Fugo";
+        case "5gac01":
+            return "Ghiaccio";
+        case "5grn01":
+            return "Giorno Giovanna";
+        case "5grn02":
+            return "Giorno Giovanna (Requiem)";
+        case "5mst01":
+            return "Guido Mista";
+        case "5nrc01":
+            return "Narancia Ghirga";
+        case "5prs01":
+            return "Prosciutto";
+        case "5psc01":
+            return "Pesci";
+        case "5psh01":
+            return "Pesci";
+        case "5ris01":
+            return "Risotto Nero";
+        case "5trs01":
+            return "Trish Una";
+        case "5ars01":
+            return "Aerosmith";
+        case "5dpo01":
+            return "Doppio";
+        case "5gex01":
+            return "Gold Experience";
+        case "5gfd01":
+            return "The Grateful Dead";
+        case "5gxr01":
+            return "Gold Experience Requiem";
+        case "5kcr01":
+            return "King Crimson";
+        case "5mdb01":
+            return "Moody Blues";
+        case "5mtl01":
+            return "Metallica";
+        case "5pph01":
+            return "Purple Haze";
+        case "5pph02":
+            return "Purple Haze Distortion";
+        case "5sfg01":
+            return "Sticky Fingers";
+        case "5spg01":
+            return "Spice Girl";
+        case "5sxp01":
+            return "Sex Pistols #1";
+        case "5sxp02":
+            return "Sex Pistols #2";
+        case "5sxp03":
+            return "Sex Pistols #3";
+        case "5sxp05":
+            return "Sex Pistols #5";
+        case "5sxp06":
+            return "Sex Pistols #6";
+        case "5sxp07":
+            return "Sex Pistols #7";
+        case "5wha01":
+            return "White Album";
+    // Part 6: Stone Ocean
+        case "6ans01":
+            return "Narciso Anasui";
+        case "6elm01":
+            return "Ermes Costello";
+        case "6fit01":
+            return "Foo Fighters";
+        case "6jln01":
+            return "Jolyne Cujoh";
+        case "6pci01":
+            return "Enrico Pucci (Final)";
+        case "6pci02":
+            return "Enrico Pucci";
+        case "6wet01":
+            return "Weather Report";
+        case "6cmo01":
+            return "C-MOON";
+        case "6ddw01":
+            return "Diver Down";
+        case "6kis01":
+            return "Kiss";
+        case "6mih01":
+            return "Made in Heaven";
+        case "6ptn01":
+            return "Foo Fighters (Stand)";
+        case "6stf01":
+            return "Stone Free";
+        case "6whs01":
+            return "Whitesnake (Final Pucci)";
+        case "6whs02":
+            return "Whitesnake";
+        case "6wrp01":
+            return "Weather Report (Stand)";
+    // Part 7: Steel Ball Run
+        case "7dio01":
+            return "Diego Brando";
+        case "7dio02":
+            return "Parallel World Diego";
+        case "7jir01":
+            return "Gyro Zeppeli";
+        case "7jny01":
+            return "Johnny Joestar";
+        case "7vtn01":
+            return "Funny Valentine";
+        case "7bbr01":
+            return "Ball Breaker";
+        case "7blt01":
+            return "Silver Bullet";
+        case "7blt02":
+            return "Silver Bullet";
+        case "7cps01":
+            return "Corpse part";
+        case "7dfc01":
+            return "D4C";
+        case "7scm01":
+            return "Diego dinosaurs";
+        case "7scm02":
+            return "Scary Monsters";
+        case "7sld01":
+            return "Slow Dancer";
+        case "7tsk01":
+            return "Tusk ACT1";
+        case "7tsk02":
+            return "Tusk ACT2";
+        case "7tsk03":
+            return "Tusk ACT3";
+        case "7tsk04":
+            return "Tusk ACT4";
+        case "7val01":
+            return "Valkyrie";
+        case "7wld01":
+            return "THE WORLD";
+    // Part 8: JoJolion
+        case "8jsk01":
+            return "Josuke Higashikata 8";
+        case "8tor01":
+            return "Toru";
+        case "8saw01":
+            return "Soft & Wet";
+        case "8wou01":
+            return "Wonder of U";
+    // If none matched
+        default:
+            return CharID;
+    }
+}
+
+string DlcIdToFullName(uint32 DlcId) {
+    switch (DlcId) {
+        case 10000:
+            return "The Animation Special Event Costumes";
+        case 10001:
+            return "Jolyne Green Dolphin Street State Prison Jacket Costume";
+        case 10002:
+            return "Rohan Kishibe First Appearance Costume";
+        case 10003:
+            return "Muhammad Avdol's Father? Costume";
+        case 10004:
+            return "Giorno Giovanna Haruno Shiobana Costume";
+        case 10005:
+            return "Risotto Nero";
+        case 10006:
+            return "Rudol von Stroheim";
+        case 10007:
+            return "Keicho Nijimura";
+        case 10008:
+            return "Parallel World Diego";
+        case 10009:
+            return "Leone Abbacchio";
+        case 10010:
+            return "Yuya Fungami";
+        case 10011:
+            return "Wonder of U (Toru)";
+        default:
+            return Str("%d", DlcId);
+    }
+}
